9cc53637f83ae0c5af67c0ac34111d7559182818
Christopher Heuer <cheuer@gmail.com>
2020-12-08 19:27:18 -0700

    Use default variant method of overriding keys for keysanity

--- scripts/init.lua
+++ scripts/init.lua
@@ -8,15 +8,7 @@ Tracker:AddMaps("maps/maps.json")
 Tracker:AddItems("items/common.json")
 Tracker:AddItems("items/dungeon_items.json")
 Tracker:AddItems("items/labels.json")
-
--- If current version is keysanity, add all keys, otherwise only add TR and GT big keys
-if not (string.find(Tracker.ActiveVariantUID, "keys")) then
-    Tracker:AddItems("items/keys_TRGT.json")
-    Tracker:AddLocations("locations/dungeons.json")
-else
-    Tracker:AddItems("items/keys.json")
-    Tracker:AddLocations("keysanity/locations/dungeons.json")
-end
+Tracker:AddItems("items/keys.json")
 
 if not (string.find(Tracker.ActiveVariantUID, "inverted")) then
     Tracker:AddLocations("locations/overworld.json")
